19b93e6ea670840369b344c06dfeb260a473a2a1
Masato Imai <mii@sfc.wide.ad.jp>
2025-04-12 11:08:58 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello, world!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("NelOS");
+
+    loop {}
+}
